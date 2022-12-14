78ef148068a1def9d97a773dd9f77d7d4a7e8374
Aneta Wawer <anwawer1@gmail.com>
2022-12-14 19:55:57 +0100

    show boards corrected

--- src/Board/SquareStatus.java
+++ src/Board/SquareStatus.java
@@ -1,7 +1,7 @@
 package Board;
 
 public enum SquareStatus{
-    EMPTY("\uD83C\uDF0A"), SHIP("\uD83D\uDEA2"), HIT("\uD83D\uDD25"), MISSED("❌"), SUNK("\uD83D\uDC80"), BLOCKED("⛔");
+    EMPTY("\uD83C\uDF0A"), SHIP("\uD83D\uDEA2"), HIT("\uD83D\uDD25"), MISSED("❌"), SUNK("☠"), BLOCKED("⛔");
 
     private String character;
 
--- src/utilities/ConsoleDisplay.java
+++ src/utilities/ConsoleDisplay.java
@@ -20,11 +20,11 @@ public class ConsoleDisplay implements Display {
         char[] columns = "ABCDEFGHIJ".toCharArray();
         board.append("    ");
         while (boardsCount!=2 ) {
-            for (int row = 0; row < playerBoard.size; row++) {
-                if (row != 4) {
-                    board.append(columns[row]).append("   ");
+            for (int i=0; i< playerBoard.size; i++) {
+                if (i !=4) {
+                    board.append(i + 1).append("   ");
                 } else {
-                    board.append(columns[row]).append("    ");
+                    board.append(i + 1).append("    ");
                 }
             }
             boardsCount++;
@@ -35,24 +35,16 @@ public class ConsoleDisplay implements Display {
         board.append("\n");
 
         for (int i=0; i< playerBoard.size; i++){
-            if (i< playerBoard.size-1) {
-                board.append(i + 1).append("  ");
-            } else {
-                board.append(i + 1).append(" ");
-            }
+            board.append(columns[i]).append("  ");
             for (int j=0; j<playerBoard.size*2+6; j++){
                 if (j<10) {
                     board.append(playerBoard.getOcean()[i][j]).append("  ");
                 } else if (j>15){
                     board.append(enemyBoard.getOcean()[i][j-16]).append("  ");
                 } else if (j==11){
-                    board.append("| ");
+                    board.append(" | ");
                 } else if (j==13){
-                    if (i<playerBoard.size-1) {
-                        board.append(i + 1).append("  ");
-                    } else {
-                        board.append(i + 1).append(" ");
-                    }
+                    board.append(columns[i]).append("  ");
                 }
             }
             board.append("\n");
