3f9c9c2db4c0dc4a771a8c726536a41822a5d1df
Karolina Chalupka <k.s.chalupka@gmail.com>
2022-12-16 18:11:45 +0100

    copy of BattleshipTest

--- src/test/java/com.battleship/BattleshipTest.java
+++ src/test/java/com.battleship/BattleshipTest.java
@@ -1,4 +1,15 @@
 package com.battleship;
 
+import main.java.org.battleship.utilities.ConsoleDisplay;
 public class BattleshipTest {
+
+    @Test
+    public void shouldShowMenuOnStart() {
+        ConsoleDisplay display = new ConsoleDisplay();
+        Battleship battleship = new Battleship(display, new ConsoleInput(display));
+
+        battleship.start();
+
+        // Jak sprawdzić czy Battleshiop w metodzie start wyświetlił menu
+    }
 }
